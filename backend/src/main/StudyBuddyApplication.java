37129567d5f78571c2f6b976f05f5ad2064cf6d7
Max Quirk <45086348+thatmoosee@users.noreply.github.com>
2025-10-14 12:54:03 -0500

    Organizational Updates

--- backend/src/main/StudyBuddyApplication.java
+++
@@ -1,9 +0,0 @@
-// StudyBuddyApplication.java
-package com.studybuddy;
-
-@SpringBootApplication
-public class StudyBuddyApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(StudyBuddyApplication.class, args);
-    }
-}
